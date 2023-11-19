14f9d027b242b1c8bcc6c6a47b2dd29302dab748
Chaaivisva <126050738+Chaaivisva@users.noreply.github.com>
2023-11-19 12:34:20 +0530

    Auto stash before merge of "main" and "origin/main"
    added product display

--- inventory/invention/views.py
+++ inventory/invention/views.py
@@ -1,5 +1,13 @@
 
 from django.shortcuts import render
+from .models import *
+
 
 def home(request):
-    return render (request,"home.html")
+    products = Product.objects.all()
+    query = request.GET.get('query', '')
+
+    if query:
+       products = products.filter(name__icontains = query)
+
+    return render(request, 'home.html', {'products': products,})
